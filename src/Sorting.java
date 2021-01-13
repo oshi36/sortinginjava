63f0f20f5bf3bba29ad5ca518079e1eef2a1e4db
Oshi Gupta <oshiagupta36@gmail.com>
2021-01-13 19:15:44 +0530

    all files added

---
+++ src/Sorting.java
@@ -0,0 +1,66 @@
+import java.util.Arrays;
+
+public class Sorting {
+
+    public static void main(String[] args) {
+        int[] arr = {4,3,5,7,2};
+        // Bubble Sort
+        bubble(arr);
+        System.out.println(Arrays.toString(arr));
+
+        // Selection sort
+        selection(arr);
+        System.out.println(Arrays.toString(arr));
+
+        // Insertion sort
+        insertion(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void bubble(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 1; j < nums.length - i; j++) {
+                if (nums[j] < nums[j-1]) {
+                    swap(nums, j, j-1);
+                }
+            }
+        }
+    }
+
+
+    public static void selection(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            int last = nums.length - 1 - i;
+            int max = max_limit(nums, last);
+            swap(nums, max, last);
+        }
+    }
+
+    private static int max_limit(int[] nums, int end) {
+        int max = 0;
+        for (int i = 0; i <= end; i++) {
+            if (nums[i] > nums[max]) {
+                max = i;
+            }
+        }
+        return max;
+    }
+
+    public static void insertion(int[] nums) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i+1; j > 0; j--) {
+                if (nums[j] < nums[j-1]) {
+                    swap(nums, j-1, j);
+                } else {
+                    break;
+                }
+            }
+        }
+    }
+
+    private static void swap(int[] nums, int j, int i) {
+        int temp = nums[j];
+        nums[j] = nums[i];
+        nums[i] = temp;
+    }
+}
